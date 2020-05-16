b0d885ec6ccefd3adefde51724f8a96cc95cdc1b
himalayachaurasia <himalya.bncet@gmail.com>
2020-05-16 13:14:41 +0530

    auto indentation changed this is my second commit into the index.php file

--- index.php
+++ index.php
@@ -52,7 +52,7 @@ session_start();
 					<div align="center">
 						<a class="btn btn-success " href="admin/index.php">ADMIN LOGIN</a>
 						<marquee scrollamount="10"><h1 class="style8">MOST Welcome to Online Quiz</h1></marquee>
-						<blockquote>
+					<blockquote>
 							<p align="center"><span class="style4">WelCome to Online
 								exam. This Site will provide the quiz for various subject of interest.
 							You need to login for the take the online exam.</p>
