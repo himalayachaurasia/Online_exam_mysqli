681cf58c6bf84417268045463d83e3c44a041a4c
himalayachaurasia <himalya.bncet@gmail.com>
2020-05-16 17:18:25 +0530

    indentationchanged

--- index.php
+++ index.php
@@ -31,14 +31,14 @@ session_start();
 		{
 		echo "<h1 class='text-center bg-danger'>Welcome to Online Exam</h1>";
 				echo '<table width="28%"  border="0" align="center">
-						<tr>
-										<td width="7%" height="65" valign="bottom"><img src="image/HLPBUTT2.JPG" width="50" height="50" align="middle"></td>
-										<td width="93%" valign="bottom" bordercolor="#0000FF"> <a href="sublist.php" class="style4">Subject for Quiz </a></td>
-						</tr>
-						<tr>
-										<td height="58" valign="bottom"><img src="image/DEGREE.JPG" width="43" height="43" align="absmiddle"></td>
-										<td valign="bottom"> <a href="result.php" class="style4">Result </a></td>
-						</tr>
+									<tr>
+																<td width="7%" height="65" valign="bottom"><img src="image/HLPBUTT2.JPG" width="50" height="50" align="middle"></td>
+																<td width="93%" valign="bottom" bordercolor="#0000FF"> <a href="sublist.php" class="style4">Subject for Quiz </a></td>
+									</tr>
+									<tr>
+																<td height="58" valign="bottom"><img src="image/DEGREE.JPG" width="43" height="43" align="absmiddle"></td>
+																<td valign="bottom"> <a href="result.php" class="style4">Result </a></td>
+									</tr>
 		</table>';
 		
 				exit;
@@ -52,7 +52,7 @@ session_start();
 					<div align="center">
 						<a class="btn btn-success " href="admin/index.php">ADMIN LOGIN</a>
 						<marquee scrollamount="10"><h1 class="style8">MOST Welcome to Online Quiz</h1></marquee>
-					<blockquote>
+						<blockquote>
 							<p align="center"><span class="style4">WelCome to Online
 								exam. This Site will provide the quiz for various subject of interest.
 							You need to login for the take the online exam.</p>
